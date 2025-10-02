37de270ecc085c907d209560ed5b9c82bada3fd3
javierprofesionalwork@gmail.com <javierprofesionalwork@gmail.com>
2025-10-02 02:06:51 -0400

    Adding changes in readme

--- Python_exercices/Binary_Search_Tree/binary.py
+++ Python_exercices/Binary_Search_Tree/binary.py
@@ -9,7 +9,20 @@ def pre_order(tree,level=0):
    print(f'level {level} -> {str(tree[0])}') # root
    pre_order(tree[1],level + 1) # left son
    pre_order(tree[2],level + 1) #right son
-
+   '''
+   
+   Generame un layer para publicacion de linkedin en el q pongas unos logos super lindos tematica programacion y digas al publicao sobre mi repositorio de programacion donde hay varios ejercicios de programacion para mejorar la logica entre varios temas esta Brute Force
+Recursion
+Linear and binary searches
+Sorting algorithms: bubble sort, quick sort
+Applied recursion
+Permutations and combinatorics
+Dynamic programming (DP)
+Graphs
+Trees
+Backtracking
+And much more una imagen basicmente
+   '''
 tree=[
     'A',
     ['B',
